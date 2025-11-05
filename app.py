3957c18727cc7a5ac09a31a60cca220def0644d2
Priyanshu Hingu <priyanshuhingu@Priyanshus-MacBook-Air.local>
2025-11-05 18:59:21 +0530

    Added db.create_all() to ensure table creation

--- app.py
+++ app.py
@@ -37,6 +37,10 @@ db.init_app(app)
 login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.init_app(app)
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
 
 
 # -------------------- MODELS --------------------
